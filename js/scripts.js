eb52061d7c08c481454ced6fd23012ddd6e0f24f
Adam Baker and Suzi Rubino <adam@>
2016-10-17 13:23:29 -0700

    Completed homework

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+  $("h1").click(function() {
+    alert("This is a header.");
+    alert("YES A HEADER.");
+  });
+
+  $("p").click(function() {
+    alert("This is a paragraph.");
+    alert("YES A PARAGRAPH.");
+  });
+
+  $("img").click(function() {
+    alert("This is an image.");
+  });
+});
